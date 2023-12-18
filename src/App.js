97f64d844181279fa4f8990f5270340269947d6f
bochkova_su <bochkova_su@resanta.ru>
2023-12-19 00:03:11 +0300

    search filter

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   const [ users, setUsers ] = React.useState([])
   const [ isLoading, setLoading ] = React.useState(true)
+  const [ searchValue, setSearchValue ] = React.useState('')
 
   // При первом рендере отправляем запрос на бэкенд (используем fetch)
   React.useEffect(() => {
@@ -22,10 +23,19 @@ function App() {
     }).finally(() => setLoading(false))
   }, [])
 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
 
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading} />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
   );
