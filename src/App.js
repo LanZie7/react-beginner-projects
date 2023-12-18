1bd221768a332a917758441c4b88708106772d96
bochkova_su <bochkova_su@resanta.ru>
2023-12-19 00:47:14 +0300

    user count in success block added

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
   return (
     <div className="App">
       {success ? (
-        <Success />
+        <Success count={invitedUsers.length} />
       ) : (
         <Users
           onChangeSearchValue={onChangeSearchValue}
